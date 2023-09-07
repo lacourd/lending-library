40f34c5689fd333256ef6f1835d3a393346f831e
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-09-06 22:02:10 -0500

    Added TagController

---
+++ src/main/java/lacourd/lendinglibrary/controllers/TagController.java
@@ -0,0 +1,45 @@
+package lacourd.lendinglibrary.controllers;
+
+import lacourd.lendinglibrary.data.TagRepository;
+import lacourd.lendinglibrary.models.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+public class TagController {
+    @Autowired
+    TagRepository tagRepository;
+
+    @GetMapping
+    public String displayTags(Model model) {
+        model.addAttribute("title", "All Tags");
+        model.addAttribute("tags", tagRepository.findAll());
+        return "tags/index";
+    }
+
+    @GetMapping("create")
+    public String displayCreateTagForm(Model model) {
+        model.addAttribute("title", "Create Tag");
+        model.addAttribute(new Tag());
+        return "tags/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateTagForm(@ModelAttribute @Valid Tag tag,
+                                       Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Tag");
+            model.addAttribute(tag);
+            return "tags/create";
+        }
+
+        tagRepository.save(tag);
+        return "redirect:";
+    }
+}
