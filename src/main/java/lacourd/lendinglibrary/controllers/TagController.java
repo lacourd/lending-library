331e17383e9d3af97793c3d14f00d829905013bd
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-09-06 22:25:49 -0500

    Added missing annotations to TagController

--- src/main/java/lacourd/lendinglibrary/controllers/TagController.java
+++ src/main/java/lacourd/lendinglibrary/controllers/TagController.java
@@ -3,14 +3,18 @@ package lacourd.lendinglibrary.controllers;
 import lacourd.lendinglibrary.data.TagRepository;
 import lacourd.lendinglibrary.models.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
+@Controller
+@RequestMapping("tags")
 public class TagController {
 
     @Autowired
