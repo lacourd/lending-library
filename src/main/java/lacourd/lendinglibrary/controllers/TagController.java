8bcde58abda3c2a7914826c24f6d0432fa00e392
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-10-05 13:23:50 -0500

    Added a delete method to TagController

--- src/main/java/lacourd/lendinglibrary/controllers/TagController.java
+++ src/main/java/lacourd/lendinglibrary/controllers/TagController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -46,5 +43,15 @@ public class TagController {
 
         tagRepository.save(tag);
         return "redirect:";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteTag(@RequestParam int tagId) {
+        Tag tagToDelete = tagRepository.findById(tagId).orElse(null);
+
+        if (tagToDelete != null) {
+            tagRepository.deleteById(tagId);
+        }
+        return "tags/index";
     }
 }
